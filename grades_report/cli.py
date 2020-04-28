1fee435336819da8e30b0a9f075d2c9ac0e5a6b1
KAUTH <konpap1996@yahoo.com>
2020-04-28 03:00:36 +0300

    Create CLI
    This patch adds the basic CLI infrastructure for the Python
    program.

---
+++ grades_report/cli.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2020 KAUTH
+"""
+
+import click
+
+# TO-DO: Don't forget to add tests for the CLI
+
+
+@click.command()
+def cli():
+    """
+    A tool that shows useful statistics and generates a report about a set of
+    given grades. Provide the
+
+    Here are some examples:
+
+    1.
+
+    2.
+
+    You can find the source code at https://github.com/KAUTH/grades-report.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    cli()
